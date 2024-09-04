4821c1022e95ec8de0ad5b3a46cbcbed54cd1364
Leonel Silvera <leonelsilvera9@gmail.com>
2024-09-04 07:00:00 -0300

    styles

--- src/js/proyects.js
+++ src/js/proyects.js
@@ -1,5 +1,7 @@
 window.addEventListener("DOMContentLoaded", function() {
     videoAutoplay();
+    scrollUp();
+    seeMore();
 })
 
 function videoAutoplay() {
@@ -13,4 +15,40 @@ function videoAutoplay() {
             e.pause();
         })
     })
+}
+
+function scrollUp() {
+    const scrollbar = document.getElementById("scrollbar");
+    if(scrollbar) {
+        scrollbar.addEventListener("click", function() {
+            document.documentElement.scrollTop = 0;
+        })   
+        window.addEventListener("scroll", function() {
+            if(document.documentElement.scrollTop > 0) {
+                document.querySelector(".arrow-scroll").classList.remove("hidden");
+            } else {
+                document.querySelector(".arrow-scroll").classList.add("hidden");
+            }
+        })     
+    }
+}
+
+function seeMore() {
+    const buttonSeeMore = document.querySelectorAll(".button");
+
+    buttonSeeMore.forEach(function(button) {
+        
+        button.addEventListener("click", function(e) {
+            const article = e.target.parentElement.parentElement;
+            const elementHidden = e.target.previousElementSibling.previousElementSibling;
+            elementHidden.classList.toggle("hidden");
+            if(e.target.textContent == "Ver más") {
+                e.target.textContent = "Ver menos";
+                article.classList.add("large");
+            } else {
+                e.target.textContent = "Ver más";
+                article.classList.remove("large");
+            };
+        });
+    });
 }
